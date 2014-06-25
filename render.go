ec62a10699c9ef50dcda8081be9ef20f87bd247d
insionng <insion@live.com>
2014-06-25 16:04:29 +0800

    Martini

---
+++ render.go
@@ -0,0 +1,469 @@
+// Package render is a middleware for Martini that provides easy JSON serialization and HTML template rendering.
+//
+//  package main
+//
+//  import (
+//    "encoding/xml"
+//
+//    "github.com/go-martini/martini"
+//    "github.com/martini-contrib/render"
+//  )
+//
+//  type Greeting struct {
+//    XMLName xml.Name `xml:"greeting"`
+//    One     string   `xml:"one,attr"`
+//    Two     string   `xml:"two,attr"`
+//  }
+//
+//  func main() {
+//    m := martini.Classic()
+//    m.Use(render.Renderer()) // reads "templates" directory by default
+//
+//    m.Get("/html", func(r render.Render) {
+//      r.HTML(200, "mytemplate", nil)
+//    })
+//
+//    m.Get("/json", func(r render.Render) {
+//      r.JSON(200, "hello world")
+//    })
+//
+//    m.Get("/xml", func(r render.Render) {
+//      r.XML(200, Greeting{One: "hello", Two: "world"})
+//    })
+//
+//    m.Run()
+//  }
+package martini
+
+import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+const (
+	ContentType    = "Content-Type"
+	ContentLength  = "Content-Length"
+	ContentBinary  = "application/octet-stream"
+	ContentJSON    = "application/json"
+	ContentHTML    = "text/html"
+	ContentXHTML   = "application/xhtml+xml"
+	ContentXML     = "text/xml"
+	defaultCharset = "UTF-8"
+)
+
+// Render is a service that can be injected into a Martini handler. Render provides functions for easily writing JSON and
+// HTML templates out to a http Response.
+/*
+type Render interface {
+	// JSON writes the given status and JSON serialized version of the given value to the http.ResponseWriter.
+	JSON(status int, v interface{})
+	// HTML renders a html template specified by the name and writes the result and given status to the http.ResponseWriter.
+	HTML(status int, name string, v interface{}, htmlOpt ...HTMLRenderOptions)
+	// XML writes the given status and XML serialized version of the given value to the http.ResponseWriter.
+	XML(status int, v interface{})
+	// Data writes the raw byte array to the http.ResponseWriter.
+	WriteData(status int, v []byte)
+	// Error is a convenience function that writes an http status to the http.ResponseWriter.
+	Error(status int)
+	// Status is an alias for Error (writes an http status to the http.ResponseWriter)
+	Status(status int)
+	// Redirect is a convienience function that sends an HTTP redirect. If status is omitted, uses 302 (Found)
+	Redirect(location string, status ...int)
+	// Template returns the internal *template.Template used to render the HTML
+	Template() *template.Template
+	// Header exposes the header struct from http.ResponseWriter.
+	Header() http.Header
+}
+*/
+
+// Delims represents a set of Left and Right delimiters for HTML template rendering
+type Delims struct {
+	// Left delimiter, defaults to {{
+	Left string
+	// Right delimiter, defaults to }}
+	Right string
+}
+
+// RenderOptions is a struct for specifying configuration options for the render.Renderer middleware
+type RenderOptions struct {
+	// Directory to load templates. Default is "templates"
+	Directory string
+	// Layout template name. Will not render a layout if "". Defaults to "".
+	Layout string
+	// Extensions to parse template files from. Defaults to [".tmpl"]
+	Extensions []string
+	// Funcs is a slice of FuncMaps to apply to the template upon compilation. This is useful for helper functions. Defaults to [].
+	Funcs []template.FuncMap
+	// Delims sets the action delimiters to the specified strings in the Delims struct.
+	Delims Delims
+	// Appends the given charset to the Content-Type header. Default is "UTF-8".
+	Charset string
+	// Outputs human readable JSON
+	IndentJSON bool
+	// Outputs human readable XML
+	IndentXML bool
+	// Prefixes the JSON output with the given bytes.
+	PrefixJSON []byte
+	// Prefixes the XML output with the given bytes.
+	PrefixXML []byte
+	// Allows changing of output to XHTML instead of HTML. Default is "text/html"
+	HTMLContentType string
+}
+
+// HTMLRenderOptions is a struct for overriding some rendering RenderOptions for specific HTML call
+type HTMLRenderOptions struct {
+	// Layout template name. Overrides RenderOptions.Layout.
+	Layout string
+}
+
+type Render struct {
+	http.ResponseWriter
+	req             *http.Request
+	t               *template.Template
+	opt             RenderOptions
+	compiledCharset string
+	Data            map[string]interface{}
+}
+
+var (
+	Data = map[string]interface{}{} //初始化Data
+
+	// Included helper functions for use when rendering html
+	helperFuncs = template.FuncMap{
+		"yield": func() (string, error) {
+			return "", fmt.Errorf("yield called with no layout defined")
+		},
+		"current": func() (string, error) {
+			return "", nil
+		},
+	}
+)
+
+func prepareCharset(charset string) string {
+	if len(charset) != 0 {
+		return "; charset=" + charset
+	}
+
+	return "; charset=" + defaultCharset
+}
+
+func prepareRenderOptions(options []RenderOptions) RenderOptions {
+	var opt RenderOptions
+	if len(options) > 0 {
+		opt = options[0]
+	}
+
+	// Defaults
+	if len(opt.Directory) == 0 {
+		opt.Directory = "templates"
+	}
+	if len(opt.Extensions) == 0 {
+		opt.Extensions = []string{".html"}
+	}
+	if len(opt.HTMLContentType) == 0 {
+		opt.HTMLContentType = ContentHTML
+	}
+
+	return opt
+}
+
+func compile(options RenderOptions) *template.Template {
+	dir := options.Directory
+	t := template.New(dir)
+	t.Delims(options.Delims.Left, options.Delims.Right)
+	// parse an initial template in case we don't have any
+	template.Must(t.Parse("Martini"))
+
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		r, err := filepath.Rel(dir, path)
+		if err != nil {
+			return err
+		}
+
+		ext := getExt(r)
+
+		for _, extension := range options.Extensions {
+			if ext == extension {
+
+				buf, err := ioutil.ReadFile(path)
+				if err != nil {
+					panic(err)
+				}
+
+				name := (r[0 : len(r)-len(ext)])
+				tmpl := t.New(filepath.ToSlash(name))
+
+				// add our funcmaps
+				for _, funcs := range options.Funcs {
+					tmpl.Funcs(funcs)
+				}
+
+				// Bomb out if parse fails. We don't want any silent server starts.
+				template.Must(tmpl.Funcs(helperFuncs).Parse(string(buf)))
+				break
+			}
+		}
+
+		return nil
+	})
+
+	return t
+}
+
+func getExt(s string) string {
+	if strings.Index(s, ".") == -1 {
+		return ""
+	}
+	return "." + strings.Join(strings.Split(s, ".")[1:], ".")
+}
+
+func (r *Render) JSON(status int, v interface{}) {
+	var result []byte
+	var err error
+	if r.opt.IndentJSON {
+		result, err = json.MarshalIndent(v, "", "  ")
+	} else {
+		result, err = json.Marshal(v)
+	}
+	if err != nil {
+		http.Error(r, err.Error(), 500)
+		return
+	}
+
+	// json rendered fine, write out the result
+	r.Header().Set(ContentType, ContentJSON+r.compiledCharset)
+	r.WriteHeader(status)
+	if len(r.opt.PrefixJSON) > 0 {
+		r.Write(r.opt.PrefixJSON)
+	}
+	r.Write(result)
+}
+
+func (r *Render) JSONString(v interface{}) (string, error) {
+	var result []byte
+	var err error
+	if r.opt.IndentJSON {
+		result, err = json.MarshalIndent(v, "", "  ")
+	} else {
+		result, err = json.Marshal(v)
+	}
+	if err != nil {
+		return "", err
+	}
+	return string(result), nil
+}
+
+func (r *Render) renderBytes(name string, binding interface{}, htmlOpt ...HTMLRenderOptions) (*bytes.Buffer, error) {
+
+	opt := r.prepareHTMLRenderOptions(htmlOpt)
+
+	if len(opt.Layout) > 0 {
+		r.addYield(name, binding)
+		name = opt.Layout
+	}
+
+	out, err := r.execute(name, binding)
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+/*
+func (r *Render) HTML(status int, name string, binding interface{}, htmlOpt ...HTMLRenderOptions) {
+		opt := r.prepareHTMLRenderOptions(htmlOpt)
+	// assign a layout if there is one
+	if len(opt.Layout) > 0 {
+		r.addYield(name, binding)
+		name = opt.Layout
+	}
+
+	out, err := r.execute(name, binding)
+	if err != nil {
+		http.Error(r, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// template rendered fine, write out the result
+	r.Header().Set(ContentType, r.opt.HTMLContentType+r.compiledCharset)
+	r.WriteHeader(status)
+	r.Write(out.Bytes())
+}
+*/
+
+func (r *Render) HTML(status int, name string, binding interface{}, htmlOpt ...HTMLRenderOptions) {
+
+	out, err := r.renderBytes(name, binding, htmlOpt...)
+	if err != nil {
+		http.Error(r, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	r.Header().Set(ContentType, r.opt.HTMLContentType+r.compiledCharset)
+	r.WriteHeader(status)
+	io.Copy(r, out)
+
+}
+
+func (r *Render) HTMLString(name string, binding interface{}, htmlOpt ...HTMLRenderOptions) (string, error) {
+	if out, err := r.renderBytes(name, binding, htmlOpt...); err != nil {
+		return "", err
+	} else {
+		return out.String(), nil
+	}
+}
+
+func (r *Render) XML(status int, v interface{}) {
+	var result []byte
+	var err error
+	if r.opt.IndentXML {
+		result, err = xml.MarshalIndent(v, "", "  ")
+	} else {
+		result, err = xml.Marshal(v)
+	}
+	if err != nil {
+		http.Error(r, err.Error(), 500)
+		return
+	}
+
+	// XML rendered fine, write out the result
+	r.Header().Set(ContentType, ContentXML+r.compiledCharset)
+	r.WriteHeader(status)
+	if len(r.opt.PrefixXML) > 0 {
+		r.Write(r.opt.PrefixXML)
+	}
+	r.Write(result)
+}
+
+func (r *Render) WriteData(status int, v []byte) {
+	if r.Header().Get(ContentType) == "" {
+		r.Header().Set(ContentType, ContentBinary)
+	}
+	r.WriteHeader(status)
+	r.Write(v)
+}
+
+// Error writes the given HTTP status to the current ResponseWriter
+func (r *Render) Error(status int, message ...string) {
+	r.WriteHeader(status)
+	if len(message) > 0 {
+		r.Write([]byte(message[0]))
+	}
+}
+
+func (r *Render) Status(status int) {
+	r.WriteHeader(status)
+}
+
+func (r *Render) Redirect(location string, status ...int) {
+	code := http.StatusFound
+	if len(status) == 1 {
+		code = status[0]
+	}
+
+	http.Redirect(r, r.req, location, code)
+}
+
+func (r *Render) Template() *template.Template {
+	return r.t
+}
+
+func (r *Render) execute(name string, binding interface{}) (*bytes.Buffer, error) {
+	buf := new(bytes.Buffer)
+	return buf, r.t.ExecuteTemplate(buf, name, binding)
+}
+
+func (r *Render) addYield(name string, binding interface{}) {
+	funcs := template.FuncMap{
+		"yield": func() (template.HTML, error) {
+			buf, err := r.execute(name, binding)
+			// return safe html here since we are rendering our own template
+			return template.HTML(buf.String()), err
+		},
+		"current": func() (string, error) {
+			return name, nil
+		},
+	}
+	r.t.Funcs(funcs)
+}
+
+func (r *Render) prepareHTMLRenderOptions(htmlOpt []HTMLRenderOptions) HTMLRenderOptions {
+	if len(htmlOpt) > 0 {
+		return htmlOpt[0]
+	}
+
+	return HTMLRenderOptions{
+		Layout: r.opt.Layout,
+	}
+}
+
+// Renderer is a Middleware that maps a render.Render service into the Martini handler chain. An single variadic render.RenderOptions
+// struct can be optionally provided to configure HTML rendering. The default directory for templates is "templates" and the default
+// file extension is ".tmpl".
+//
+// If MARTINI_ENV is set to "" or "development" then templates will be recompiled on every request. For more performance, set the
+// MARTINI_ENV environment variable to "production"
+/*
+func Renderer(options ...RenderOptions) martini.Handler {
+	opt := prepareRenderOptions(options)
+	cs := prepareCharset(opt.Charset)
+	t := compile(opt)
+	return func(res http.ResponseWriter, req *http.Request, c martini.Context) {
+		var tc *template.Template
+		if martini.Env == martini.Dev {
+			// recompile for easy development
+			tc = compile(opt)
+		} else {
+			// use a clone of the initial template
+			tc, _ = t.Clone()
+		}
+		c.MapTo(&Render{res, req, tc, opt, cs, TmplData{}, time.Time{}}, (*Render)(nil))
+	}
+}
+*/
+
+func Renderer(res http.ResponseWriter, req *http.Request, c Context, options ...RenderOptions) *Render {
+
+	if Data["RequestStartTime"] == nil {
+		Data["RequestStartTime"] = time.Now()
+	}
+
+	/*
+		Data["TmplLoadTimes"] = func(startTime time.Time) string {
+			if startTime.IsZero() {
+				return ""
+			}
+			return fmt.Sprint(time.Since(startTime).Nanoseconds()/1e6) + "ms"
+		}
+	*/
+	opt := prepareRenderOptions(options)
+	cs := prepareCharset(opt.Charset)
+	t := compile(opt)
+	var tc *template.Template
+
+	if Env == Dev {
+
+		tc = compile(opt)
+	} else {
+
+		tc, _ = t.Clone()
+	}
+
+	return &Render{res, req, tc, opt, cs, Data}
+
+	//c.Map(rd)
+	//c.MapTo(rd.Data, (*map[string]interface{})(nil))
+	//c.MapTo(rd, (*Render)(nil))
+
+}
